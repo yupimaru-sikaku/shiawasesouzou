805e476c46464b890ca561602a862104a10dd90e
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2021-11-09 15:39:31 +0900

    all

--- app/javascript/packs/companies/new.js
+++
@@ -1,9 +0,0 @@
-const companyRegistration = document.querySelector(".company_registration");
-const isAgree = document.querySelector("#is_agree");
-
-companyRegistration.addEventListener("click", (e) => {
-  if (!isAgree.checked) {
-    alert("会員規約に同意しなければ登録できません");
-    e.preventDefault();
-  }
-});
